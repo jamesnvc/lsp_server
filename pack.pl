cae079963450667f760dbe87ec0d1b2d94e629c0
James N. V. Cash <james.nvc@gmail.com>
2019-04-04 09:58:02 -0400

    Add pack.pl

---
+++ pack.pl
@@ -0,0 +1,7 @@
+name(lsp_server).
+title('A Prolog LSP Server').
+version('1.0.0').
+author('James N. V. Cash', 'james.cash@occasionallycogent.com').
+home('https://github.com/jamesnvc/prolog-lsp').
+download('https://github.com/jamesnvc/prolog-lsp/releases/*.zip').
+provides(lsp_server).
