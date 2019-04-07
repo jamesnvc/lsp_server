ac85b6505db118835190d91de742051d3cbed719
James N. V. Cash <james.nvc@gmail.com>
2019-04-07 13:47:56 -0400

    Fix urls in pcak.pl & bump version

--- pack.pl
+++ pack.pl
@@ -1,7 +1,7 @@
 name(lsp_server).
 title('A Prolog LSP Server').
-version('1.1.1').
+version('1.1.2').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
-home('https://github.com/jamesnvc/prolog-lsp').
-download('https://github.com/jamesnvc/prolog-lsp/releases/*.zip').
+home('https://github.com/jamesnvc/lsp_server').
+download('https://github.com/jamesnvc/lsp_server/releases/*.zip').
 provides(lsp_server).
