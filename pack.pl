78f95dafa3da476e7a1ea7b37c1ad8a7da567069
James N. V. Cash <james.cash@occasionallycogent.com>
2025-02-04 14:16:19 -0500

    Bump version

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(lsp_server).
 title('A Prolog LSP Server').
-version('2.5.0').
+version('2.5.1').
 author('James N. V. Cash', 'james.cash@occasionallycogent.com').
 home('https://github.com/jamesnvc/lsp_server').
 download('https://github.com/jamesnvc/lsp_server/releases/*.zip').
