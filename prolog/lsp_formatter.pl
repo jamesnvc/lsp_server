2175745c8033e429b7db66f7d1e575a44b4b9497
James N. V. Cash <james.cash@occasionallycogent.com>
2025-03-20 14:19:13 -0400

    WIP: Flatten out term positions

--- prolog/lsp_formatter.pl
+++ prolog/lsp_formatter.pl
@@ -53,38 +53,86 @@ read_term_positions(Path, TermsWithPositions) :-
 
 reified_format_for_file(Path, Reified) :-
     read_term_positions(Path, TermsWithPos),
+    expand_term_positions(TermsWithPos, Reified0),
     file_lines_start_end(Path, LinesStartEnd),
     InitState = _{last_line: 1, last_char: 0, line_bounds: LinesStartEnd},
-    expand_term_positions(InitState, TermsWithPos, Reified).
+    add_whitespace_terms(InitState, Reified0, Reified).
 
-expand_term_positions(_, [], []).
-expand_term_positions(State0, [InfoDict|Rest], Expanded0) :-
-    % use byte count of comment position vs term position to decide which to output first?
-    % flatten out the comments and terms?
+add_whitespace_terms(_State, [], []) :- !.
+add_whitespace_terms(State, [Term|Terms], Out) :-
+    arg(1, Term, TermStart),
+    stream_position_at_offset(State.line_bounds, TermStart, Pos),
+    % hm...but after a term start, should (usually?) be an open paren,
+    % not a space, similiarly between a term's args, list's items, etc
+    sync_position_whitespace(State, Pos, Out, Out1),
+    Out1 = [Term|Out2],
+    arg(2, Term, TermEnd),
+    stream_position_at_offset(State.line_bounds, TermEnd, EndPos),
+    update_state_position(State, EndPos, State1),
+    add_whitespace_terms(State1, Terms, Out2).
+
+expand_term_positions([], []).
+expand_term_positions([InfoDict|Rest], Expanded0) :-
     ( InfoDict.comments \= []
-    -> expand_comments_positions(State0, State, InfoDict.comments, Expanded0, Expanded)
-    ;  ( Expanded = Expanded0, State = State0, Expanded = Expanded0 ) ),
+    -> expand_comments_positions(InfoDict.comments, Expanded0, Expanded1)
+    ;  Expanded1 = Expanded0 ),
 
-    TermPos = InfoDict.pos,
-    sync_position_whitespace(State, TermPos, Expanded, Expanded1),
-    Expanded1 = [term(InfoDict.term)|Expanded2],
+    Term = InfoDict.term,
+    expand_subterm_positions(Term, InfoDict.subterm, Expanded1, Expanded2),
+    expand_term_positions(Rest, Expanded2).
 
-    subterm_end_position(State.line_bounds, InfoDict.subterm, TermEndPos),
+expand_comments_positions([], Tail, Tail) :- !.
+expand_comments_positions([Comment|Rest], Expanded, Tail) :-
+    expand_comment_positions(Comment, Expanded, Tail0),
+    expand_comments_positions(Rest, Tail0, Tail).
 
-    update_state_position(State, TermEndPos, State1),
-    expand_term_positions(State1, Rest, Expanded2).
-
-expand_comments_positions(State, State, [], Tail, Tail) :- !.
-expand_comments_positions(State0, State, [Comment|Rest], Expanded, Tail) :-
-    expand_comment_positions(State0, State1, Comment, Expanded, Tail0),
-    expand_comments_positions(State1, State, Rest, Tail0, Tail).
-
-expand_comment_positions(State0, State1, CommentPos-Comment, Expanded, ExpandedTail) :-
-    sync_position_whitespace(State0, CommentPos, Expanded, Expanded1),
-    Expanded1 = [comment(Comment)|ExpandedTail],
+expand_comment_positions(CommentPos-Comment, Expanded, ExpandedTail) :-
     term_end_position(Comment, CommentEndPosRel),
     increment_stream_position(CommentPos, CommentEndPosRel, CommentEndPos),
-    update_state_position(State0, CommentEndPos, State1).
+    stream_position_data(char_count, CommentPos, From),
+    stream_position_data(char_count, CommentEndPos, To),
+    Expanded = [comment(From, To, Comment)|ExpandedTail].
+
+expand_subterm_positions(Term, term_position(From, To, FFrom, FTo, SubPoses), Expanded, ExTail) =>
+    % using functor/4 to allow round-tripping zero-arity functors
+    functor(Term, Func, Arity, TermType),
+    % way to tell if term is parenthesized?
+    ( From \= FFrom
+    -> debug(lsp(format), "From(~q) \\= FFrom(~q); Term ~q ", [From, FFrom, Term])
+    ; true ),
+    Expanded = [term_begin(FFrom, FTo, Func, TermType)|ExpandedTail0],
+    expand_term_subterms_positions(Term, Arity, 1, SubPoses,
+                                   ExpandedTail0, ExpandedTail1),
+    succ(To0, To),
+    ExpandedTail1 = [term_end(To0, To)|ExTail].
+expand_subterm_positions(Term, From-To, Expanded, Tail) =>
+    Expanded = [simple(From, To, Term)|Tail].
+expand_subterm_positions(Term, list_position(From, To, Elms, HasTail), Expanded, Tail) =>
+    assertion(is_listish(Term)),
+    Expanded = [list_begin(From, To)|Expanded1],
+    expand_list_subterms_positions(Term, Elms, Expanded1, Expanded2),
+    succ(To0, To),
+    (  HasTail = none
+    -> Expanded2 = [list_end(To0, To)|Tail]
+    % need to expand HasTail too?
+    ;  Expanded2 = [list_tail(HasTail), list_end|Tail]).
+
+is_listish(L) :- \+ var(L), !.
+is_listish([]).
+is_listish([_|_]).
+
+expand_list_subterms_positions([], [], Tail, Tail) :- !.
+expand_list_subterms_positions([Term|Terms], [Pos|Poses], Expanded, Tail) :-
+    expand_subterm_positions(Term, Pos, Expanded, Expanded1),
+    expand_list_subterms_positions(Terms, Poses, Expanded1, Tail).
+
+expand_term_subterms_positions(_Term, _Arity, _Arg, [], Tail, Tail) :- !.
+expand_term_subterms_positions(Term, Arity, Arg, [SubPos|Poses], Expanded, ExpandedTail) :-
+    assertion(between(1, Arity, Arg)),
+    arg(Arg, Term, SubTerm),
+    expand_subterm_positions(SubTerm, SubPos, Expanded, Expanded0),
+    succ(Arg, Arg1),
+    expand_term_subterms_positions(Term, Arity, Arg1, Poses, Expanded0, ExpandedTail).
 
 increment_stream_position(StartPos, RelPos, EndPos) :-
     stream_position_data(char_count, StartPos, StartCharCount),
@@ -135,11 +183,11 @@ file_offset_line_position(LineCharMap, CharCount, Line, LinePosition) :-
 subterm_end_position(LineCharMap, term_position(_From, To, _FFrom, _FTo, _), EndPos) =>
     % TODO probably do need to split out the functor & the whole thing...somewhere
     % breaking the rules, building an opaque term
-    CharCount = To,
-    ByteCount = To, % need to check for multibyte...
-    file_offset_line_position(LineCharMap, To, LineCount, LinePosition),
-    EndPos = '$stream_position_data'(CharCount, LineCount, LinePosition, ByteCount).
+    stream_position_at_offset(LineCharMap, To, EndPos).
 subterm_end_position(LineCharMap, _From-To, EndPos) =>
+    stream_position_at_offset(LineCharMap, To, EndPos).
+
+stream_position_at_offset(LineCharMap, To, EndPos) :-
     CharCount = To,
     ByteCount = To, % need to check for multibyte...
     file_offset_line_position(LineCharMap, To, LineCount, LinePosition),
