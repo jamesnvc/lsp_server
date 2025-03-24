c8c1badde4a322256b550f9244a198d9e45d1703
James N. V. Cash <james.cash@occasionallycogent.com>
2025-03-24 13:08:36 -0400

    WIP: indentation rules

--- prolog/lsp_formatter.pl
+++ prolog/lsp_formatter.pl
@@ -9,6 +9,7 @@ Module for formatting Prolog source code
 */
 
 :- use_module(library(readutil), [ read_file_to_string/3 ]).
+:- use_module(library(macros)).
 
 :- use_module(lsp_formatter_parser, [ reified_format_for_file/2,
                                       emit_reified/2 ]).
@@ -34,20 +35,85 @@ apply_format_rules(Content, Formatted) :-
     phrase(formatter_rules, Content, Formatted).
 
 formatter_rules -->
-    comments_exactly_one_space.
+    commas_exactly_one_space,
+    correct_indentation(_{state: [toplevel]}).
 
-comments_exactly_one_space([], Out) => Out = [].
-comments_exactly_one_space([white(_), comma|InRest], Out) =>
-    comments_exactly_one_space([comma|InRest], Out).
-comments_exactly_one_space([comma, white(_)|InRest], Out) =>
+commas_exactly_one_space([], Out) => Out = [].
+commas_exactly_one_space([white(_), comma|InRest], Out) =>
+    commas_exactly_one_space([comma|InRest], Out).
+commas_exactly_one_space([comma, white(_)|InRest], Out) =>
     Out = [comma, white(1)|OutRest],
-    comments_exactly_one_space(InRest, OutRest).
-comments_exactly_one_space([comma, Next|InRest], Out), Next \= white(_), Next \= newline =>
+    commas_exactly_one_space(InRest, OutRest).
+commas_exactly_one_space([comma, Next|InRest], Out), Next \= white(_), Next \= newline =>
     Out = [comma, white(1), Next|OutRest],
-    comments_exactly_one_space(InRest, OutRest).
-comments_exactly_one_space([Other|Rest], Out) =>
+    commas_exactly_one_space(InRest, OutRest).
+commas_exactly_one_space([Other|Rest], Out) =>
     Out = [Other|OutRest],
-    comments_exactly_one_space(Rest, OutRest).
+    commas_exactly_one_space(Rest, OutRest).
+
+#define(toplevel_indent, 4).
+
+correct_indentation(State0,
+                    [term_begin(Func, Arity, Parens)|InRest],
+                    [term_begin(Func, Arity, Parens)|OutRest]) :-
+    indent_state_top(State0, toplevel),
+    Func = ':-', !,
+    indent_state_push(State0, declaration, State1),
+    correct_indentation(State1, InRest, OutRest).
+correct_indentation(State0,
+                    [term_begin(Func, Arity, Parens)|InRest],
+                    [term_begin(Func, Arity, Parens)|OutRest]) :-
+    indent_state_top(State0, toplevel), !,
+    indent_state_push(State0, defn_head, State1),
+    correct_indentation(State1, InRest, OutRest).
+correct_indentation(State0,
+                    [term_begin(Neckish, A, P)|InRest],
+                    [term_begin(Neckish, A, P)|OutRest]) :-
+    memberchk(Neckish, [':-', '=>', '-->']),
+    indent_state_top(State0, defn_head), !,
+    indent_state_push(State0, defn_body, State1),
+    correct_indentation(State1, InRest, OutRest).
+correct_indentation(State0, [newline|InRest], [newline|Out]) :-
+    indent_state_top(State0, defn_body), !,
+    indent_state_push(State0, defn_body_indent, State1),
+    correct_indentation(State1, InRest, Out).
+correct_indentation(State0, [In|InRest], Out) :-
+    indent_state_top(State0, defn_body_indent), !,
+    ( In = white(_)
+    -> correct_indentation(State0, InRest, Out)
+    ;  ( indent_state_pop(State0, State1),
+         Out = [white(#toplevel_indent), In|OutRest],
+         correct_indentation(State1, InRest, OutRest) )).
+correct_indentation(State0, [In|InRest], [In|OutRest]) :-
+    functor(In, Name, _Arity, _Type),
+    atom_concat(_, '_begin', Name), !,
+    indent_state_push(State0, In, State1),
+    correct_indentation(State1, InRest, OutRest).
+correct_indentation(State0, [In|InRest], [In|OutRest]) :-
+    indent_state_top(State0, defn_head),
+    In = term_end(_, S), S \= toplevel, !,
+    % should state change to be like "expect head" or something?
+    correct_indentation(State0, InRest, OutRest).
+correct_indentation(State0, [In|InRest], [In|OutRest]) :-
+    functor(In, Name, _Arity, _Type),
+    atom_concat(_, '_end', Name), !,
+    indent_state_pop(State0, State1),
+    correct_indentation(State1, InRest, OutRest).
+correct_indentation(State0, [In|InRest], [In|OutRest]) :-
+    correct_indentation(State0, InRest, OutRest).
+correct_indentation(_, [], []) :-
+    debug(lsp(format), "INDENTING - DONE", []).
+
+indent_state_top(State, Top) :-
+    _{state: [Top|_]} :< State.
+
+indent_state_push(State0, NewTop, State1) :-
+    _{state: Stack} :< State0,
+    put_dict(state, State0, [NewTop|Stack], State1).
+
+indent_state_pop(State0, State1) :-
+    _{state: [_|Rest]} :< State0,
+    put_dict(state, State0, Rest, State1).
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Create a List of Edits from the Original and Formatted Lines
