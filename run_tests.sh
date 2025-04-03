da2a366d568e0fb553e098451ff010747bff4355
James N. V. Cash <james.cash@occasionallycogent.com>
2025-04-03 15:20:20 -0400

    Make run_tests.sh script run all tests in directory

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
-exec swipl -l test/formatter.plt -g plunit:run_tests -t halt
+set -euo pipefail
+
+declare -a load_test_files
+for f in test/*.plt; do
+    load_test_files+=( "-l" )
+    load_test_files+=( "${f}" )
+done
+exec swipl ${load_test_files[@]} -g plunit:run_tests -t halt
