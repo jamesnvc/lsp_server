b08413d1f4ae644225510207fa6c8a89bc9eb0b0
Gideon <eignnx@gmail.com>
2025-06-09 15:43:10 -0400

    mvp diagnostic for existence_error

--- prolog/lsp_checking.pl
+++ prolog/lsp_checking.pl
@@ -13,9 +13,11 @@ Module for checking Prolog source files for errors and warnings.
 :- use_module(library(debug), [debug/3]).
 :- use_module(library(lists), [member/2]).
 :- use_module(library(prolog_xref), [xref_clean/1, xref_source/1]).
+:- use_module(library(dcg/high_order)).
 
 :- include('_lsp_path_add.pl').
-:- use_module(lsp(lsp_utils), [clause_variable_positions/3]).
+:- use_module(lsp(lsp_utils), [clause_variable_positions/3,
+                               clause_import_position/4]).
 
 :- dynamic user:thread_message_hook/3.
 :- multifile user:thread_message_hook/3.
@@ -39,60 +41,84 @@ check_errors(Path, Errors) :-
         erase(Ref)
     ),
     nb_getval(checking_errors, ErrList),
-    once(expand_errors(Path, ErrList, Errors-Errors)).
+    once(phrase(expand_errors(Path, ErrList), Errors)).
 
-expand_errors(Path, [e(singletons(_, SingletonVars), warning, _, ClauseLine, _)|InErrs],
-              OutErrs-Tail0) :- !,
-    clause_variable_positions(Path, ClauseLine, VariablePoses),
-    list_to_assoc(VariablePoses, VarPoses),
-    findall(
-        NewErr,
-        ( member(VarName, SingletonVars),
-          atom_length(VarName, VarLen),
-          get_assoc(VarName, VarPoses, [position(Line, Char)]),
-          EndChar is Char + VarLen,
-          format(string(Msg), "Singleton variable ~w", [VarName]),
-          NewErr = _{severity: 2,
-                     source: "prolog_xref",
-                     range: _{start: _{line: Line, character: Char},
-                              end: _{line: Line, character: EndChar}},
-                     message: Msg} ),
-        Tail0,
-        Tail1
-    ),
-    expand_errors(Path, InErrs, OutErrs-Tail1).
-expand_errors(Path, [e(_, silent, _, _, _)|InErr], OutErrs-Tail) :- !,
-    expand_errors(Path, InErr, OutErrs-Tail).
-expand_errors(Path, [e(_Term, error, Lines, _, _)|InErrs], OutErrs-[Err|Tail]) :-
-    Lines = [url(_File:Line1:Col1), _, _, Msg0], !,
-    ( Msg0 = Fmt-Params
+blah(VarPoses, VarName) -->
+    {
+        atom_length(VarName, VarLen),
+        get_assoc(VarName, VarPoses, [position(Line, Char)]),
+        EndChar is Char + VarLen,
+        format(string(Msg), "Singleton variable ~w", [VarName])
+    },
+    [ _{severity: 2,
+        source: "prolog_xref",
+        range: _{start: _{line: Line, character: Char},
+                end:   _{line: Line, character: EndChar}},
+        message: Msg
+    } ].
+
+expand_errors(
+    Path,
+    [e(singletons(_, SingletonVars), warning, _, ClauseLine, _)|InErrs]
+) --> !,
+    {
+        clause_variable_positions(Path, ClauseLine, VariablePoses),
+        list_to_assoc(VariablePoses, VarPoses)
+    },
+    sequence(blah(VarPoses), SingletonVars),
+    expand_errors(Path, InErrs).
+expand_errors(
+    Path,
+    [e(error(existence_error(file, FileSpec), _), _Kind, _Lines, Line, _Char)|InErrs]
+) --> !,
+    {
+        clause_import_position(Path, Line, FileSpec, Span),
+        format(string(Msg), "Existence error for file `~p`", [FileSpec])
+    },
+    [ _{severity: 2,
+        source: "prolog_xref",
+        range: Span,
+        % range: _{start: _{line: StartLine, character: Char},
+        %          end:   _{line: StartLine, character: EndChar}},
+        message: Msg
+    } ],
+    expand_errors(Path, InErrs).
+expand_errors(Path, [e(_, silent, _, _, _)|InErrs]) --> !,
+    expand_errors(Path, InErrs).
+expand_errors(Path, [e(_Term, error, Lines, _, _)|InErrs]) -->
+    { Lines = [url(_File:Line1:Col1), _, _, Msg0] },
+    !,
+    { Msg0 = Fmt-Params
     -> format(string(Msg), Fmt, Params)
-    ;  text_to_string(Msg0, Msg) ),
-    succ(Line0, Line1), ( succ(Col0, Col1) ; Col0 = 0 ),
-    Err = _{severity: 1,
-            source: "prolog_xref",
-            range: _{start: _{line: Line0, character: Col0},
-                     end: _{line: Line1, character: 0}},
-            message: Msg
+    ;  text_to_string(Msg0, Msg),
+       succ(Line0, Line1), ( succ(Col0, Col1) ; Col0 = 0 )
     },
-    expand_errors(Path, InErrs, OutErrs-Tail).
-expand_errors(Path, [e(_Term, Kind, Lines, _, _)|InErr], OutErrs-[Err|Tail]) :-
-    kind_level(Kind, Level),
-    Lines = ['~w:~d:~d: '-[Path, Line1, Char1]|Msgs0], !,
-    maplist(expand_error_message, Msgs0, Msgs),
-    atomic_list_concat(Msgs, Msg),
-    succ(Line0, Line1),
-    ( succ(Char0, Char1) ; Char0 = 0 ),
-    Err = _{severity: Level,
-            source: "prolog_xref",
-            range: _{start: _{line: Line0, character: Char0},
-                     end: _{line: Line1, character: 0}},
-            message: Msg
+    [_{severity: 1,
+       source: "prolog_xref",
+       range: _{start: _{line: Line0, character: Col0},
+                end:   _{line: Line1, character: 0}},
+       message: Msg
+    }],
+    expand_errors(Path, InErrs).
+expand_errors(Path, [e(_Term, Kind, Lines, _, _)|InErr]) -->
+    {
+        kind_level(Kind, Level),
+        Lines = ['~w:~d:~d: '-[Path, Line1, Char1]|Msgs0], !,
+        maplist(expand_error_message, Msgs0, Msgs),
+        atomic_list_concat(Msgs, Msg),
+        succ(Line0, Line1),
+        ( succ(Char0, Char1) ; Char0 = 0 )
     },
-    expand_errors(Path, InErr, OutErrs-Tail).
-expand_errors(Path, [_Msg|InErr], OutErrs-Tail) :- !,
-    expand_errors(Path, InErr, OutErrs-Tail).
-expand_errors(_, [], _-[]) :- !.
+    [_{severity: Level,
+       source: "prolog_xref",
+       range: _{start: _{line: Line0, character: Char0},
+                end:   _{line: Line1, character: 0}},
+       message: Msg
+    }],
+    expand_errors(Path, InErr).
+% Skip unhandleable ones:
+expand_errors(Path, [_Msg|InErr]) --> !, expand_errors(Path, InErr).
+expand_errors(_, []) --> [].
 
 expand_error_message(Format-Args, Formatted) :-
     !, format(string(Formatted), Format, Args).
